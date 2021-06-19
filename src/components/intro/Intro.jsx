2df3b250c8998e38a8ac1d2810b0630d2aa584ca
Dakota States <dakota.states@gmail.com>
2021-06-18 22:24:37 -0700

    Intro setup, with stock image and ityped

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,35 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from 'react'
 
 export default function Intro(){
+
+  const textRef = useRef();
+
+  useEffect(()=>{
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      backSpeed: 60,
+      strings: ['Software Engineer', 'Designer!']})
+  },[])
   return(
     <div className="intro" id="intro">
-    Intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/man.png" alt=""/>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Dakota States!</h1>
+          <h3>Freelancer <span ref={textRef}></span></h3>
+        </div>
+        <a href="#portfolio">
+          <img src="assets/arrow.png" alt=""/>
+        </a>
+      </div>
     </div>
   )
 }
