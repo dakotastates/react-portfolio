bfca4d3df1153d6e9045c48c5cace3d46f53b203
Dakota States <dakota.states@gmail.com>
2021-06-19 00:19:55 -0700

    portfolio section, add data, and portfolio list

---
+++ src/data.js
@@ -0,0 +1,164 @@
+export const featuredPortfolio = [
+  {
+    id: 1,
+    title: "Covid-19 Tracker CLI",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 2,
+    title: "The Neighborhood",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 3,
+    title: "Recipe Viewer",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 4,
+    title: "FixStarter",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 5,
+    title: "Library of Things",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 6,
+    title: "Application Tracker",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+];
+
+export const webPortfolio = [
+  {
+    id: 1,
+    title: "Web App",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 2,
+    title: "The Neighborhood",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 3,
+    title: "Recipe Viewer",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 4,
+    title: "FixStarter",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 5,
+    title: "Library of Things",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 6,
+    title: "Application Tracker",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+];
+
+export const mobilePortfolio = [
+  {
+    id: 1,
+    title: "Mobile App",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 2,
+    title: "The Neighborhood",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 3,
+    title: "Recipe Viewer",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 4,
+    title: "FixStarter",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 5,
+    title: "Library of Things",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 6,
+    title: "Application Tracker",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+];
+
+export const designPortfolio = [
+  {
+    id: 1,
+    title: "Design app",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 2,
+    title: "The Neighborhood",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 3,
+    title: "Recipe Viewer",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 4,
+    title: "FixStarter",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 5,
+    title: "Library of Things",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 6,
+    title: "Application Tracker",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+];
+
+export const contentPortfolio = [
+  {
+    id: 1,
+    title: "Social Media App",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 2,
+    title: "The Neighborhood",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 3,
+    title: "Recipe Viewer",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 4,
+    title: "FixStarter",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 5,
+    title: "Library of Things",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+  {
+    id: 6,
+    title: "Application Tracker",
+    img: "https://www.fda.gov/files/covid19-1600x900.jpg"
+  },
+];
